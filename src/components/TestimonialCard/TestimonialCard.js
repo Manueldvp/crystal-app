b40534c9f1a22b4b4dde097f9f17028bfc19513c
manueldvp <manuelbyto@gmail.com>
2024-08-05 22:43:28 -0400

    cambios para los testimonios

--- src/components/TestimonialCard/TestimonialCard.js
+++ src/components/TestimonialCard/TestimonialCard.js
@@ -27,24 +27,7 @@ function Reviews() {
       fetchData();
     }, []);
 
-  // const [reviews, setReviews] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchReviews = async () => {
-  //     // const proxyUrl = 'http://localhost:3000/'
-  //     const apiKey = process.env.GOOGLE_MAPS_API_KEY
-  //     const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ4dhTKFS5z4UR3fEa8TxPeGw&fields=reviews&key=${apiKey}`
-  //     try {
-  //       const response = await fetch(url);
-  //       const data = await response.json();
-  //       setReviews(data.result.reviews);
-  //       console.log(reviews);
-  //     } catch (error) {
-  //       console.error('Error fetching reviews:', error);
-  //     }
-  //   };
-  //   fetchReviews();
-  // }, []);
+  
 
   return (
     
